2cad26e6c03e6cfb684f6ab860722cedff0ca8d4
oliveiha <oliveiha@gmail.com>
2019-06-09 17:07:39 -0300

    script chart museum .sh

---
+++ devops/03-chartmuseum.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+helm lint backend-scm/
+helm push backend-scm/ questcode
+
+helm lint backend-user/
+helm push backend-user/ questcode
+
+helm lint frontend/
+helm push frontend/ questcode
+
+helm repo update
