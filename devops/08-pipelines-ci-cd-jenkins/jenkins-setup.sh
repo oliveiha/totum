7278fce4c5e0d29611b758b9f45abe282a97e1af
oliveiha <oliveiha@gmail.com>
2019-06-11 21:44:08 -0300

    atualização script instal jenkins

--- devops/08-pipelines-ci-cd-jenkins/jenkins-setup.sh
+++ devops/08-pipelines-ci-cd-jenkins/jenkins-setup.sh
@@ -3,3 +3,5 @@ kubectl apply -f jenkins-pv-pvc.yaml
 
 helm install --name jenkins --set Persistence.ExistingClaim=jenkins --set Master.ServiceType=NodePort --set Master.NodePort=30808 --namespace devops stable/jenkins
 helm install --name jenkins --set Persistence.ExistingClaim=jenkinn2 stable/jenkins --namespace devops
+kubectl create rolebinding sa-devops-role-clusteradmin --clusterrole=cluster-admin --serviceaccount=devops:default --namespace=devops
+kubectl create rolebinding sa-devops-role-clusteradmin-kubesystem --clusterrole=cluster-admin --serviceaccount=devops:default --namespace=kube-system
